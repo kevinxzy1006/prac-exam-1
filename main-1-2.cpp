1b253c69e3f0a8a61b0e1700d3d6fc61181bd224
kevinxzy1006 <161264609+kevinxzy1006@users.noreply.github.com>
2024-08-08 17:52:06 +0930

    main-1-2.cpp

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -5,18 +5,9 @@ extern void modifyArray(double *array, int size, double value);
 int main() {
     double myArray[] = {1.0, 2.0, 3.0, 4.0, 5.0};
     int size = sizeof(myArray) / sizeof(myArray[0]);
-
     double addValue = 1.5;
-
-    std::cout << "Original array: ";
-    for (int i = 0; i < size; ++i) {
-        std::cout << myArray[i] << " ";
-    }
-    std::cout << std::endl;
-
     modifyArray(myArray, size, addValue);
 
-    std::cout << "Modified array: ";
     for (int i = 0; i < size; ++i) {
         std::cout << myArray[i] << " ";
     }
